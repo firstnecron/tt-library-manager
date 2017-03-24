8ef73f2c96e60e1ca4a2f126f3bd23f2dc5e4cd2
Justin Russo <j.russo@outlook.com>
2017-03-24 02:08:33 -0400

    Create data service to retrieve all books, loans, or patrons

---
+++ src/client/scripts/services/data-service.js
@@ -0,0 +1,50 @@
+(function () {
+	'use strict';
+
+	angular.module('app')
+		.service('DataService', function ($location, $http) {
+			const baseUrl = $location.host;
+
+			/*
+				Books
+			 */
+			this.getBooks = function () {
+				return new Promise((resolve, reject) => {
+					$http.get(`${baseUrl}/api/books`)
+						.then(successResponse => {
+							resolve(successResponse.data);
+						}, errorResponse => {
+							reject(errorResponse);
+						});
+				});
+			};
+
+			/*
+				Loans
+			 */
+			this.getLoans = function () {
+				return new Promise((resolve, reject) => {
+					$http.get(`${baseUrl}/api/loans`)
+						.then(successResponse => {
+							resolve(successResponse.data);
+						}, errorResponse => {
+							reject(errorResponse);
+						});
+				});
+			};
+
+			/*
+				Patrons
+			 */
+			this.getPatrons = function () {
+				return new Promise((resolve, reject) => {
+					$http.get(`${baseUrl}/api/patrons`)
+						.then(successResponse => {
+							resolve(successResponse.data);
+						}, errorResponse => {
+							reject(errorResponse);
+						});
+				});
+			};
+		});
+})();

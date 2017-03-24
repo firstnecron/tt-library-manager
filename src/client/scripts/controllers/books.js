5017432ab997c7d2008763774d805dbe95ab88a0
Justin Russo <j.russo@outlook.com>
2017-03-24 04:00:33 -0400

    Fill books table with data from database

--- src/client/scripts/controllers/books.js
+++ src/client/scripts/controllers/books.js
@@ -3,7 +3,11 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('BooksController', function () {
-			console.log('Books controller');
+		.controller('BooksController', function ($scope, DataService) {
+			$scope.books = [];
+			DataService.getBooks()
+				.then(books => {
+					$scope.books = books;
+				});
 		});
 })();

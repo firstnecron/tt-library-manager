abd3836d68ba5813ce613bc861b83e3e51bd4dac
Justin Russo <j.russo@outlook.com>
2017-03-29 16:41:24 -0400

    Fix data binding for books array

--- src/client/scripts/controllers/books.js
+++ src/client/scripts/controllers/books.js
@@ -4,10 +4,11 @@
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
 		.controller('BooksController', function ($scope, DataService) {
-			$scope.books = [];
 			DataService.getBooks()
 				.then(books => {
-					$scope.books = books;
+					$scope.$apply(() => {
+						$scope.books = books;
+					});
 				});
 		});
 })();

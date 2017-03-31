9fae4e484742067db74684040f307ae0e1112f9a
Justin Russo <j.russo@outlook.com>
2017-03-31 06:54:13 -0400

    Update data from database whenever navigating to the books state

--- src/client/scripts/controllers/books/books.js
+++ src/client/scripts/controllers/books/books.js
@@ -3,12 +3,30 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('BooksController', function ($scope, DataService) {
-			DataService.getBooks()
-				.then(books => {
-					$scope.$apply(() => {
-						$scope.books = books;
+		.controller('BooksController', function ($rootScope, $scope, DataService) {
+			function getBooks() {
+				DataService.getBooks()
+					.then(books => {
+						$scope.$apply(() => {
+							$scope.books = books;
+						});
 					});
-				});
+			}
+			getBooks();
+
+			// Listen to state changes to see if it is books
+			// If so, update (re-retrieve) books from database
+			const stateListener = $rootScope.$on('$stateChangeSuccess', (event, toState) => {
+				if (toState.name.toLowerCase() === 'books') {
+					getBooks();
+				}
+			});
+
+			// When we leave books, remove stateListener
+			$scope.$on('$destroy', () => {
+				if (angular.isDefined(stateListener)) {
+					stateListener();
+				}
+			});
 		});
 })();

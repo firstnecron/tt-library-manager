1c7bfac621e80c022b29d898ab40d723cc368a53
Justin Russo <j.russo@outlook.com>
2017-03-31 06:30:26 -0400

    Create book modal to display new / edit book form

--- src/client/scripts/controllers/books/book.js
+++ src/client/scripts/controllers/books/book.js
@@ -3,24 +3,91 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('BookController', function ($scope, $state, $document, DataService) {
-			const bookID = $state.params.id;
-			if (bookID) {
-				// Editing book
-				DataService.getBook(bookID)
-					.then(bookData => {
-						$scope.book = bookData;
-					});
-			} else {
-				// New book
-			}
-
+		.controller('BookController', function ($scope, $state, $document, $interval, DataService) {
 			const modalOptions = {
 				backdrop: 'static',
 				keyboard: false
 			};
-			// eslint-disable-next-line
-			const modal = new Modal($document[0].querySelector('#bookModal'), modalOptions);
-			modal.show();
+			const modalElement = $document[0].querySelector('#bookModal');
+			const modal = new Modal(modalElement, modalOptions); // eslint-disable-line
+			$scope.cancelClicked = false;
+
+			const bookID = $state.params.id;
+			let editing = false;
+
+			function hideModal(callback) {
+				modalElement.addEventListener('hidden.bs.modal', callback);
+				modal.hide();
+			}
+
+			$scope.save = function () {
+				function handleSuccess(/* data */) {
+					hideModal(() => {
+						$state.go('books');
+					});
+				}
+
+				function handleError(error) {
+					console.error(error);
+				}
+
+				if (editing) {
+					DataService.updateBook(bookID, $scope.book)
+						.then(handleSuccess)
+						.catch(handleError);
+				} else {
+					DataService.addBook($scope.book)
+						.then(handleSuccess)
+						.catch(handleError);
+				}
+			};
+
+			$scope.cancel = function () {
+				$scope.cancelClicked = true;
+				$scope.confirmButtonText = 'Confirm Cancel 5';
+				let i = 4;
+				// After 5 seconds, revert
+				let interval = $interval(() => {
+					console.log(i);
+					if (i === 0) {
+						$scope.cancelClicked = false;
+						$interval.cancel(interval);
+						interval = undefined;
+					} else {
+						$scope.confirmButtonText = `Confirm Cancel ${i}`;
+						i--;
+					}
+				}, 1000);
+
+				$scope.$on('$destroy', () => {
+					if (angular.isDefined(interval)) {
+						$interval.cancel(interval);
+						interval = undefined;
+					}
+				});
+			};
+
+			$scope.confirm = function () {
+				hideModal(() => {
+					$state.go('books');
+				});
+			};
+
+			if (bookID) {
+				// Editing book
+				DataService.getBook(bookID)
+					.then(bookData => {
+						if (bookData) {
+							editing = true;
+							$scope.$apply(() => {
+								$scope.book = bookData;
+								modal.show();
+							});
+						}
+					});
+			} else {
+				// New book
+				modal.show();
+			}
 		});
 })();

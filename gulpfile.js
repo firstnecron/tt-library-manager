a973fbc53c953cff87a24f83ae38ad1163f58054
Justin Russo <j.russo@outlook.com>
2017-03-31 04:33:15 -0400

    Add bootstrap.native dependency

--- gulpfile.js
+++ gulpfile.js
@@ -74,6 +74,7 @@ gulp.task('vendor:scripts', () => {
 		`${options.npm}/angular-animate/angular-animate.js`,
 		`${options.npm}/angular-ui-router/release/angular-ui-router.js`,
 		`${options.npm}/angular-touch/angular-touch.js`,
+		`${options.npm}/bootstrap.native/build.js`,
 		`${options.npm}/angular-ui-bootstrap/dist/ui-bootstrap.js`
 	])
 		.pipe(plugins.sourcemaps.init())

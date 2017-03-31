3d7fd568a6ac30b8b5b75dd6b189befe41463bd6
Justin Russo <j.russo@outlook.com>
2017-03-31 12:04:43 -0400

    Create Patrons Controller

--- src/client/scripts/controllers/patrons/patrons.js
+++ src/client/scripts/controllers/patrons/patrons.js
@@ -3,7 +3,30 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('PatronsController', function () {
-			console.log('Patrons controller');
+		.controller('PatronsController', function ($rootScope, $scope, DataService) {
+			function getPatrons() {
+				DataService.getPatrons()
+					.then(patrons => {
+						$scope.$apply(() => {
+							$scope.patrons = patrons;
+						});
+					});
+			}
+			getPatrons();
+
+			// Listen to state changes to see if it is patrons
+			// If so, update (re-retrieve) patrons from database
+			const stateListener = $rootScope.$on('$stateChangeSuccess', (event, toState) => {
+				if (toState.name.toLowerCase() === 'patrons') {
+					getPatrons();
+				}
+			});
+
+			// When we leave books, remove stateListener
+			$scope.$on('$destroy', () => {
+				if (angular.isDefined(stateListener)) {
+					stateListener();
+				}
+			});
 		});
 })();

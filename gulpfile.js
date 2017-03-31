4df89901799890fa002a2a2773b46b11744ac109
Justin Russo <j.russo@outlook.com>
2017-03-31 04:42:43 -0400

    Fix bootstrap native path

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task('vendor:scripts', () => {
 		`${options.npm}/angular-animate/angular-animate.js`,
 		`${options.npm}/angular-ui-router/release/angular-ui-router.js`,
 		`${options.npm}/angular-touch/angular-touch.js`,
-		`${options.npm}/bootstrap.native/build.js`
+		`${options.npm}/bootstrap.native/dist/bootstrap-native.js`
 	])
 		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.concat('vendor.min.js'))

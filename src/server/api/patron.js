925ca85292825e0b610ff405dd212e52e8354002
Justin Russo <j.russo@outlook.com>
2017-03-31 14:28:19 -0400

    Add CRUD routes to patron api

--- src/server/api/patron.js
+++ src/server/api/patron.js
@@ -20,6 +20,49 @@ function routes() {
 					res.status(500).json(error);
 				});
 		});
+
+	/**
+	 * Middleware for single patron
+	 * Used for get, put, and remove
+	 * Retrieves 1 patron and attatches to req object
+	 */
+	router.use('/:patronId', (req, res, next) => {
+		Patron.findById(req.params.patronId)
+			.then(patron => {
+				if (patron) {
+					req.patron = patron;
+					return next();
+				}
+				return res.status(404).send('No patron found.');
+			})
+			.catch(error => {
+				res.status(500).json(error);
+			});
+	});
+
+	router.route('/:patronId')
+		.get((req, res) => {
+			res.json(req.patron);
+		})
+		.put((req, res) => {
+			req.patron.update(req.body)
+				.then(patron => {
+					res.json(patron);
+				})
+				.catch(error => {
+					res.status(500).json(error);
+				});
+		})
+		.delete((req, res) => {
+			req.patron.destroy()
+				.then(() => {
+					res.status(204).send('Patron removed');
+				})
+				.catch(error => {
+					res.status(500).json(error);
+				});
+		});
+
 	return router;
 }
 

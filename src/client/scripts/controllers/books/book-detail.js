6a22604ca69eb6c8f5d3a489100dac4b9c2aee11
Justin Russo <j.russo@outlook.com>
2017-04-01 07:45:34 -0400

    Create book and patron detail pages

--- src/client/scripts/controllers/books/book-detail.js
+++ src/client/scripts/controllers/books/book-detail.js
@@ -3,7 +3,49 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('BookDetailController', function () {
-			console.log('Book Detail controller');
+		.controller('BookDetailController', function ($scope, $state, DataService) {
+			const bookID = $state.params.id;
+			$scope.updated = false;
+
+			$scope.save = function () {
+				if ($scope.bookDetailForm.$pristine) {
+					$scope.updated = true;
+				} else {
+					DataService.updateBook(bookID, $scope.book)
+						.then(() => {
+							$scope.updated = true;
+							$scope.$apply();
+						})
+						.catch(() => {
+							$scope.updated = false;
+							$scope.$apply();
+						});
+				}
+			};
+
+			$scope.goBack = function () {
+				$state.go('books');
+			};
+
+			$scope.returnBook = function () {
+			};
+
+			if (bookID) {
+				// Editing book
+				DataService.getBook(bookID)
+					.then(bookData => {
+						$scope.book = bookData;
+						$scope.$apply();
+						if (bookData) {
+							return DataService.getLoansForBook(bookData.id);
+						}
+					})
+					.then(loansForBook => {
+						$scope.loans = loansForBook;
+						$scope.$apply();
+					});
+			} else {
+				$state.go('books');
+			}
 		});
 })();
--- src/client/scripts/controllers/patrons/patron-detail.js
+++ src/client/scripts/controllers/patrons/patron-detail.js
@@ -3,7 +3,49 @@
 
 	angular.module('app')
 	// eslint-disable-next-line prefer-arrow-callback
-		.controller('PatronDetailController', function () {
-			console.log('Patron Detail controller');
+		.controller('PatronDetailController', function ($scope, $state, DataService) {
+			const patronID = $state.params.id;
+			$scope.updated = false;
+
+			$scope.save = function () {
+				if ($scope.patronDetailForm.$pristine) {
+					$scope.updated = true;
+				} else {
+					DataService.updatePatron(patronID, $scope.patron)
+						.then(() => {
+							$scope.updated = true;
+							$scope.$apply();
+						})
+						.catch(() => {
+							$scope.updated = false;
+							$scope.$apply();
+						});
+				}
+			};
+
+			$scope.goBack = function () {
+				$state.go('patrons');
+			};
+
+			$scope.returnBook = function () {
+			};
+
+			if (patronID) {
+				// Editing patron
+				DataService.getPatron(patronID)
+					.then(patronData => {
+						$scope.patron = patronData;
+						$scope.$apply();
+						if (patronData) {
+							return DataService.getLoansForPatron(patronData.id);
+						}
+					})
+					.then(loansForPatron => {
+						$scope.loans = loansForPatron;
+						$scope.$apply();
+					});
+			} else {
+				$state.go('patrons');
+			}
 		});
 })();

510c2099883bb358dea29140c82cb995e62a902e
Justin Russo <j.russo@outlook.com>
2017-04-02 17:57:39 -0400

    use confirm-cancel-button directive

--- src/client/scripts/controllers/books/book.js
+++ src/client/scripts/controllers/books/book.js
@@ -50,30 +50,6 @@
 				}
 			};
 
-			$scope.cancel = function () {
-				$scope.cancelClicked = true;
-				$scope.confirmButtonText = 'Confirm Cancel 5';
-				let i = 4;
-				// After 5 seconds, revert
-				let interval = $interval(() => {
-					if (i === 0) {
-						$scope.cancelClicked = false;
-						$interval.cancel(interval);
-						interval = undefined;
-					} else {
-						$scope.confirmButtonText = `Confirm Cancel ${i}`;
-						i--;
-					}
-				}, 1000);
-
-				$scope.$on('$destroy', () => {
-					if (angular.isDefined(interval)) {
-						$interval.cancel(interval);
-						interval = undefined;
-					}
-				});
-			};
-
 			$scope.confirm = function () {
 				hideModal(() => {
 					goBack();
